227ccd551deba41601b95045b668de3ffa472f24
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-03-25 10:10:29 +0300

    item-dto-1: реаоизована класс-dto для [Item]

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,24 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ItemDto {
+    private Long id;
+
+    @NotBlank(message = "Название не может быть пустым")
+    private String name;
+
+    @NotBlank(message = "Описание не может быть пустым")
+    private String description;
+
+    @NotNull(message = "Статус доступности должен быть указан")
+    private Boolean available;
 }
