a3c370b2c1584ed24f0c7ed13cc93b821ddbe640
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-03-25 09:49:42 +0300

    u-model-1: реаоизована модель для [User]

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,22 @@
 package ru.practicum.shareit.user;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
+    private Long id;
+
+    @NotBlank(message = "Имя не может быть пустым")
+    private String name;
+
+    @NotBlank(message = "Email не может быть пустым")
+    @Email(message = "Неверный формат email")
+    private String email;
 }
