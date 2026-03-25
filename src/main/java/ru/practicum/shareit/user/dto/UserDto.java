5da9f11ae19d03c8bb785513c796da026b5cdaa9
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-03-25 10:10:41 +0300

    user-dto-1: реаоизована класс-dto для [Item]

---
+++ src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -0,0 +1,22 @@
+package ru.practicum.shareit.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDto {
+    private Long id;
+
+    @NotBlank(message = "Имя не может быть пустым")
+    private String name;
+
+    @NotBlank(message = "Email не может быть пустым")
+    @Email(message = "Неверный формат email")
+    private String email;
+}
