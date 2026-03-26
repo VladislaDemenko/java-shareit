4ba84b1325d3368f1c82c9cf97d23bacf5f46bf9
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-03-26 18:03:53 +0300

    User-controller: реаоизован контроллер [User]

--- src/main/java/ru/practicum/shareit/user/UserController.java
+++ src/main/java/ru/practicum/shareit/user/UserController.java
@@ -4,80 +4,47 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.user.dto.UserDto;
 
 import javax.validation.Valid;
-import java.util.*;
+import java.util.List;
 
 @Slf4j
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
-    private final Map<Long, User> users = new HashMap<>();
-    private long currentId = 1;
+    private final UserService userService;
 
     @GetMapping
-    public List<User> getAll() {
+    public List<UserDto> getAll() {
         log.info("GET /users");
-        return new ArrayList<>(users.values());
+        return userService.getAll();
     }
 
     @GetMapping("/{id}")
-    public User getById(@PathVariable Long id) {
+    public UserDto getById(@PathVariable Long id) {
         log.info("GET /users/{}", id);
-        User user = users.get(id);
-        if (user == null) {
-            throw new IllegalArgumentException("Пользователь не найден");
-        }
-        return user;
+        return userService.getById(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public User create(@Valid @RequestBody User user) {
+    public UserDto create(@Valid @RequestBody UserDto userDto) {
         log.info("POST /users");
-
-        if (users.values().stream().anyMatch(u -> u.getEmail().equals(user.getEmail()))) {
-            throw new IllegalArgumentException("Пользователь с таким email уже существует");
-        }
-
-        user.setId(currentId++);
-        users.put(user.getId(), user);
-        return user;
+        return userService.create(userDto);
     }
 
     @PatchMapping("/{id}")
-    public User update(@PathVariable Long id, @RequestBody User userUpdates) {
+    public UserDto update(@PathVariable Long id, @RequestBody UserDto userDto) {
         log.info("PATCH /users/{}", id);
-
-        User existingUser = users.get(id);
-        if (existingUser == null) {
-            throw new IllegalArgumentException("Пользователь не найден");
-        }
-
-        if (userUpdates.getEmail() != null && !userUpdates.getEmail().equals(existingUser.getEmail())) {
-            if (users.values().stream().anyMatch(u -> u.getEmail().equals(userUpdates.getEmail()))) {
-                throw new IllegalArgumentException("Пользователь с таким email уже существует");
-            }
-            existingUser.setEmail(userUpdates.getEmail());
-        }
-
-        if (userUpdates.getName() != null) {
-            existingUser.setName(userUpdates.getName());
-        }
-
-        return existingUser;
+        return userService.update(id, userDto);
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long id) {
         log.info("DELETE /users/{}", id);
-
-        if (!users.containsKey(id)) {
-            throw new IllegalArgumentException("Пользователь не найден");
-        }
-
-        users.remove(id);
+        userService.delete(id);
     }
 }
