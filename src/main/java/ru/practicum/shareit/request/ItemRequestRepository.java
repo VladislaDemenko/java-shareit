948b9235f39f6d9bf64e46571cfd7e81f7cd4d2b
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-03-26 17:59:08 +0300

    Request-repository: реаоизован Репозиторий [Request]

---
+++ src/main/java/ru/practicum/shareit/request/ItemRequestRepository.java
@@ -0,0 +1,32 @@
+package ru.practicum.shareit.request;
+
+import org.springframework.stereotype.Repository;
+import java.util.*;
+
+@Repository
+public class ItemRequestRepository {
+    private final Map<Long, ItemRequest> requests = new HashMap<>();
+    private long currentId = 1;
+
+    public ItemRequest save(ItemRequest request) {
+        if (request.getId() == null) {
+            request.setId(currentId++);
+        }
+        requests.put(request.getId(), request);
+        return request;
+    }
+
+    public Optional<ItemRequest> findById(Long id) {
+        return Optional.ofNullable(requests.get(id));
+    }
+
+    public List<ItemRequest> findAllByRequesterId(Long requesterId) {
+        return requests.values().stream()
+                .filter(request -> request.getRequesterId().equals(requesterId))
+                .toList();
+    }
+
+    public List<ItemRequest> findAll() {
+        return new ArrayList<>(requests.values());
+    }
+}
