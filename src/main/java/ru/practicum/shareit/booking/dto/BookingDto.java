632995627449ce2f5f09d3ef29462ad2c0e7b481
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-06 15:22:33 +0300

    Booking: Обновлен класс-DTO для более корректной работы

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
+
+import javax.validation.constraints.Future;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
@@ -13,8 +17,15 @@ import java.time.LocalDateTime;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class BookingDto {
     Long id;
+
+    @NotNull(message = "Дата начала не может быть пустой")
+    @FutureOrPresent(message = "Дата начала не может быть в прошлом")
     LocalDateTime start;
+
+    @NotNull(message = "Дата окончания не может быть пустой")
+    @Future(message = "Дата окончания должна быть в будущем")
     LocalDateTime end;
+
     Long itemId;
     Long bookerId;
     String status;
