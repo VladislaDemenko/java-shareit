4dd220c7f4845d5c8be524c8f32d87fc8c57c82c
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-06 15:25:12 +0300

    itemRequest: Изменены сущности для более корректной работы

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -5,15 +5,27 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
+@Entity
+@Table(name = "requests")
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ItemRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @Column(nullable = false, length = 1000)
     String description;
+
+    @Column(name = "requester_id", nullable = false)
     Long requesterId;
+
+    @Column(name = "created_date", nullable = false)
     LocalDateTime created;
 }
