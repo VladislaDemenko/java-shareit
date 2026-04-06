89bcb7f1bc8fadb95f404726115e53f7d8002a22
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-06 15:21:21 +0300

    Booking: Добавлены аннотации [@Entity]

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -5,24 +5,38 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
+@Entity
+@Table(name = "bookings")
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+
+    @Column(name = "start_date", nullable = false)
     LocalDateTime start;
+
+    @Column(name = "end_date", nullable = false)
     LocalDateTime end;
+
+    @Column(name = "item_id", nullable = false)
     Long itemId;
+
+    @Column(name = "booker_id", nullable = false)
     Long bookerId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
     BookingStatus status;
 
     public enum BookingStatus {
-        WAITING,
-        APPROVED,
-        REJECTED,
-        CANCELED
+        WAITING, APPROVED, REJECTED, CANCELED
     }
 }
