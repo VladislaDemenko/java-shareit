499ef09eef6d0a592c6c9eb0264c2f99ba7ba388
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-11 16:46:51 +0300

    Server-UserDto: Обновлен  UserDto для передачи данных пользователя

--- server/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ server/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -6,20 +6,12 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class UserDto {
     Long id;
-
-    @NotBlank(message = "Имя не может быть пустым")
     String name;
-
-    @NotBlank(message = "Email не может быть пустым")
-    @Email(message = "Неверный формат email")
     String email;
 }
