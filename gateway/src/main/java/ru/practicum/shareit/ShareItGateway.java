ae4cb01c7f9be3ff7f0596b025b4d69da37dee9e
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-11 16:02:03 +0300

    Настроены зависимости

--- gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
+++ gateway/src/main/java/ru/practicum/shareit/ShareItGateway.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShareItServer {
-	public static void main(String[] args) {
-		SpringApplication.run(ShareItServer.class, args);
-	}
+public class ShareItGateway {
+    public static void main(String[] args) {
+        SpringApplication.run(ShareItGateway.class, args);
+    }
 }
