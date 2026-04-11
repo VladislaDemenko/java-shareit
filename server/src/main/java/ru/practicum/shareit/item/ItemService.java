276621923aab8b6d575a29f147741ccd884767bc
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-11 16:54:00 +0300

    Server-ItemRep: Обновлен интерфейс ItemService

--- server/src/main/java/ru/practicum/shareit/item/ItemService.java
+++ server/src/main/java/ru/practicum/shareit/item/ItemService.java
@@ -9,7 +9,7 @@ public interface ItemService {
     ItemDto create(Long userId, ItemDto itemDto);
     ItemDto update(Long userId, Long itemId, ItemDto itemDto);
     ItemDto getById(Long userId, Long itemId);
-    List<ItemDto> getAllByOwner(Long userId);
-    List<ItemDto> search(String text);
+    List<ItemDto> getAllByOwner(Long userId, Integer from, Integer size);
+    List<ItemDto> search(String text, Integer from, Integer size);
     CommentDto addComment(Long userId, Long itemId, CommentDto commentDto);
 }
