7d127cc97bc16a83fab774676a300c085121f6d0
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-11 16:45:56 +0300

    Server-ErrorHandler: Обновлен  ErrorHandler для обработки исключений в server

--- server/src/main/java/ru/practicum/shareit/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/ErrorHandler.java
@@ -5,9 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
-import javax.validation.ConstraintViolationException;
 import java.util.Map;
 
 @Slf4j
@@ -19,20 +17,6 @@ public class ErrorHandler {
     public Map<String, String> handleIllegalArgumentException(IllegalArgumentException e) {
         log.error("Validation error: {}", e.getMessage());
         return Map.of("error", e.getMessage());
-    }
-
-    @ExceptionHandler(ConstraintViolationException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleConstraintViolationException(ConstraintViolationException e) {
-        log.error("Constraint violation: {}", e.getMessage());
-        return Map.of("error", e.getMessage());
-    }
-
-    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public Map<String, String> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
-        log.error("Argument type mismatch: {}", e.getMessage());
-        return Map.of("error", "Unknown state: UNSUPPORTED_STATUS");
     }
 
     @ExceptionHandler(Exception.class)
