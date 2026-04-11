7d32878389a521ddffee755a61f548cf94a48faf
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-11 16:56:40 +0300

    Server-BookingService: Обновлен интерфейс BookingService

--- server/src/main/java/ru/practicum/shareit/booking/BookingService.java
+++ server/src/main/java/ru/practicum/shareit/booking/BookingService.java
@@ -9,8 +9,8 @@ public interface BookingService {
     BookingDto create(Long userId, BookingDto bookingDto);
     BookingDto approve(Long userId, Long bookingId, Boolean approved);
     BookingDto getById(Long userId, Long bookingId);
-    List<BookingDto> getAllByUser(Long userId, String state);
-    List<BookingDto> getAllByOwner(Long userId, String state);
+    List<BookingDto> getAllByUser(Long userId, String state, Integer from, Integer size);
+    List<BookingDto> getAllByOwner(Long userId, String state, Integer from, Integer size);
     BookingShortDto getLastBooking(Long itemId);
     BookingShortDto getNextBooking(Long itemId);
     boolean hasUserBookedItem(Long userId, Long itemId);
