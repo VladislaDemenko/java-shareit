5beb80f34130e20f851b81b2aa0aa71aafbb8c49
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-11 16:53:48 +0300

    Server-ItemRep: Обновлен ItemRepository с методами поиска по requestId

--- server/src/main/java/ru/practicum/shareit/item/ItemRepository.java
+++ server/src/main/java/ru/practicum/shareit/item/ItemRepository.java
@@ -1,5 +1,6 @@
 package ru.practicum.shareit.item;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,10 +9,14 @@ import ru.practicum.shareit.item.model.Item;
 import java.util.List;
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
-    List<Item> findAllByOwnerIdOrderByIdAsc(Long ownerId);
+    List<Item> findAllByOwnerIdOrderByIdAsc(Long ownerId, Pageable pageable);
 
     @Query("SELECT i FROM Item i WHERE i.available = true AND " +
             "(LOWER(i.name) LIKE LOWER(CONCAT('%', :text, '%')) OR " +
             "LOWER(i.description) LIKE LOWER(CONCAT('%', :text, '%')))")
-    List<Item> searchAvailable(@Param("text") String text);
+    List<Item> searchAvailable(@Param("text") String text, Pageable pageable);
+
+    List<Item> findAllByRequestId(Long requestId);
+
+    List<Item> findAllByRequestIdIn(List<Long> requestIds);
 }
