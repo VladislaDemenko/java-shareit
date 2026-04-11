84afcc19ef73c692f2caa9ada57817c2e65a1a8f
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-11 16:55:22 +0300

    Server-BookingDto: Обновлен BookingDto для передачи данных бронирования

--- server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ server/src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -6,9 +6,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
-import javax.validation.constraints.Future;
-import javax.validation.constraints.FutureOrPresent;
-import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
 @Data
@@ -17,15 +14,8 @@ import java.time.LocalDateTime;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class BookingDto {
     Long id;
-
-    @NotNull(message = "Дата начала не может быть пустой")
-    @FutureOrPresent(message = "Дата начала не может быть в прошлом")
     LocalDateTime start;
-
-    @NotNull(message = "Дата окончания не может быть пустой")
-    @Future(message = "Дата окончания должна быть в будущем")
     LocalDateTime end;
-
     Long itemId;
     Long bookerId;
     String status;
