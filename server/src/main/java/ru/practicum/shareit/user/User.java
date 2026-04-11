20ad277354b6da9edce02f5f4dbcdf48ad9dfe21
Demenko Vladislav <vvladislademenko@yandex.ru>
2026-04-11 16:46:26 +0300

    Server-User: Обновлен  сущность User с полями id, name, email

--- server/src/main/java/ru/practicum/shareit/user/User.java
+++ server/src/main/java/ru/practicum/shareit/user/User.java
@@ -7,8 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
 
 import javax.persistence.*;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
 
 @Data
 @Entity
@@ -21,12 +19,9 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
 
-    @NotBlank(message = "Имя не может быть пустым")
     @Column(nullable = false)
     String name;
 
-    @NotBlank(message = "Email не может быть пустым")
-    @Email(message = "Неверный формат email")
     @Column(nullable = false, unique = true)
     String email;
 }
